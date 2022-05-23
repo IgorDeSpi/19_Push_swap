e0e2df647f20f0ae10349ac0f3e51f439d45e2fc
Igor De spirlet <ide-spir@fu-r2-p7.s19.be>
2022-05-23 16:13:25 +0200

    adding ft_atoi

--- libft_0.c
+++ libft_0.c
@@ -6,9 +6,62 @@
 /*   By: ide-spir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/23 13:47:40 by ide-spir          #+#    #+#             */
-/*   Updated: 2022/05/23 13:47:52 by ide-spir         ###   ########.fr       */
+/*   Updated: 2022/05/23 16:13:16 by ide-spir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+void	ft_putstr_fd(char *s, int fd)
+{
+	int	i;
+
+	i = 0;
+	if (!s)
+		return ;
+	while (s[i] != '\0')
+		ft_putchar_fd(s[i++], fd);
+}
+
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
+static long	check_atoi_inter(int sign, long nbr, char c)
+{
+	nbr = nbr * 10 + c - '0';
+	if (nbr > 2147483648)
+		error();
+	if (nbr == 2147483648 && sign > 0)
+		error();
+	return (nbr);
+}
+
+int	ft_atoi(const char *str)
+{
+	int		i;
+	long	nbr;
+	int		sign;
+
+	i = 0;
+	sign = 1;
+	nbr = 0;
+	while ((str[i] == ' '))
+		++i;
+	if (str[i] == '-')
+		sign = -1;
+	if (str[i] == '+' || str[i] == '-')
+	{
+		if (str[++i] < '0' || str[i] > '9')
+			error();
+	}
+	else if (str[i] < '0' || str[i] > '9')
+		error();
+	while (str[i] >= '0' && str[i] <= '9')
+		nbr = check_ft_atoi_inter(sign, nbr, str[i++]);
+	if ((str[i] == ' ') || (str[i] == '\0'))
+		return (sign * nbr);
+	error();
+	return (0);
+}
