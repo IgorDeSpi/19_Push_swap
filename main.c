ab0731832fcdc1b900c756d013f0b1d034459074
Igor De spirlet <ide-spir@fu-r2-p7.s19.be>
2022-06-02 13:59:17 +0200

    Main.c + creation of makefile

--- main.c
+++ main.c
@@ -6,8 +6,29 @@
 /*   By: ide-spir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 12:09:11 by ide-spir          #+#    #+#             */
-/*   Updated: 2022/06/02 10:33:16 by ide-spir         ###   ########.fr       */
+/*   Updated: 2022/06/02 13:57:49 by ide-spir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_push	*push;
+
+	push = malloc(sizeof(t_push));
+	if (!push)
+		return (1);
+	push->a = NULL;
+	push->b = NULL;
+	if (init_push(argc, argv, push))
+	{
+		if (is_sorted(push->a))
+			return (0);
+		sort(push);
+	}
+	ft_ps_lstclear(&(push->a));
+	ft_ps_lstclear(&(push->b));
+	free(push);
+	return (0);
+}
